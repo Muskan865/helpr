c075b7a540464c6ebf54f7e80aa2daf1d18eb287
Iffatttt <156417458+Iffatttt@users.noreply.github.com>
2026-04-27 12:18:18 +0500

    corrected getactivejobs query to remove active jobs after rating

--- backend/controllers/requesterController.js
+++ backend/controllers/requesterController.js
@@ -209,8 +209,16 @@ exports.getRequesterActiveJobs = async (req, res) => {
         JOIN worker w ON (j.worker_id = w.user_id OR j.worker_id = w.id)
         JOIN users u ON w.user_id = u.id
         LEFT JOIN bid b ON b.request_id = sr.id AND (b.worker_id = w.user_id OR b.worker_id = w.id) AND b.status = 'accepted'
-        WHERE sr.requester_id = @requesterId
-          AND LOWER(j.status) != 'completed'
+        WHERE sr.requester_id = @requesterId 
+          AND( LOWER(j.status) != 'completed' 
+          OR  ( LOWER(j.status) == 'completed' 
+            AND NOT EXISTS (
+                SELECT 1 FROM rating_review rr
+                WHERE rr.reviewer_id = @requesterId
+                  AND rr.reviewee_id = w.user_id
+                  )
+                )
+            )
       `);
 
     res.json(result.recordset);
